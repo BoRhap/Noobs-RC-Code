4898dc0d28b554b8b325f65f33befcf9f0b955fc
fox <2603000@qq.com>
2023-03-13 11:27:46 +0800

    202303131127

---
+++ Balance/insectSwarmCheck.js
@@ -0,0 +1,68 @@
+getComponent = () => {
+
+    const insectSwarmId = 48468;
+    const insectSwarConfig = [
+        {
+            bossName: "Algalon the Observer",
+            encounterId: 757,
+            targetId: 32871,
+            phrase: null,
+        },
+    ];
+
+    const fightsRes = reportGroup.fights;
+    //遍历Config数组,搜索战斗
+    for (let k of insectSwarConfig) {
+        for (let j of fightsRes) {
+            if (j.encounterId === k.encounterId) {
+                k.fightId = fightsRes.indexOf(j);
+            }
+        }
+    }
+
+
+    //获取平衡德队员ID
+
+    for (let k of insectSwarConfig) {
+        const balancePlayerRes = fightsRes[k.fightId].combatantInfoEvents.flatMap(combatantInfo => combatantInfo.source.subType === 'Druid' ? combatantInfo : []);
+        // return balancePlayerRes
+        for (let j of balancePlayerRes) {
+
+            if (j.talentPoints[0] > j.talentPoints[2]) {
+                key = Object(j.source.name);
+                k[key] = 0;
+            }
+
+        }
+    }
+
+    //计算覆盖率
+    for (let k of insectSwarConfig){
+        const eventRes = fightsRes[k.fightId].events;
+        applyRes = eventRes.flatMap(event => (event.type === "applydebuff")? event:[]);
+        removeRes = eventRes.flatMap(event => (event.type === "removedebuff")? event:[]);
+        // return buffRes;
+        insectSwarmApplyRes = applyRes.flatMap(event => event.ability.id === insectSwarmId ? event:[]);
+        insectSwarmRemoveRes = removeRes.flatMap(event => event.ability.id === insectSwarmId ? event:[]);
+        const startTime = fightsRes[k.fightId].startTime;
+        const endTime = fightsRes[k.fightId].endTime;
+        const combatTime = endTime-startTime;
+
+        let insectSwarmSumTime = 0;
+        length = insectSwarmApplyRes.length;
+
+        for (i=0;i<length;i++){
+
+            insectSwarmSumTime = insectSwarmSumTime + (insectSwarmRemoveRes[i].timestamp-insectSwarmApplyRes[i].timestamp);
+        }
+
+        let insectSwarmPercent = ((insectSwarmSumTime/combatTime)*100).toFixed(2);
+        key = Object(insectSwarmApplyRes[0].source.name);
+        k[key] = insectSwarmPercent;
+
+    }
+
+    return insectSwarConfig;
+
+
+}
